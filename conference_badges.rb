7427eda3d9ccf592e673ed344249f11cc1be572e
Jack Madden <jck13mad@gmail.com>
2020-01-30 00:12:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,30 +5,38 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
+
+
 def batch_badge_creator(array)
-  new_array = []
-  array.each do
-    new_array.push("Hello, my name is #{name}.")
+  nuarray = []
+  array.each do |name|
+    nuarray.push("Hello, my name is #{name}.")
   end
-  return new_array
+  return nuarray
 end
+
+# batch_badge_creator(people)
+
+
 
 def assign_rooms(array)
-  new_array = []
+  nuarray = []
   counter = 1
   array.each do |name|
-    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-    counter +=1
+    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
   end
-  return new_array
+  return nuarray
 end
 
+# assign_rooms(people)
+
 def printer(array)
-  batch_badge_creator.each(array) do |id|
-    return id
-  end
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
   
   assign_rooms(array).each do |id|
-    return id
+    puts id 
   end
-end
+end 
